c8cc4dc83c71310a485b7cf4970679da1dcfa42c
itasouza <itasouza@yahoo.com.br>
2021-12-03 23:54:00 -0300

    Curso VScode

---
+++ curso/src/app/01-edicion-basica/03-comentarios.ts
@@ -0,0 +1,24 @@
+/*
+    Objetivo:
+        Comentar un bloque de código
+
+    Tips:
+        ⌘ /
+        
+        Alt + Shfit + A (comenta e descomenta)
+*/
+
+const a = 10; 
+const b = 20;
+const c = { a, b };
+
+/* 
+const a = 30;
+const b = 40;
+const c = { a, b }; */
+
+
+console.log( a, b, c );
+
+// Resultado final
+// Sólo dejar un bloque comentado
